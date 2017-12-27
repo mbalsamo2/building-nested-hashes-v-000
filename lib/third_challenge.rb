ae9339b7d36c657a995fd0834ccc852251721195
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-27 23:10:40 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,10 +14,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
-      ]
+      :hero_friends => []
    },
    :capulet => {
       :patriarch => {
@@ -29,10 +26,7 @@ def third_challenge
       :heroine => {
         name: "Juliet", age: "15", status: "alive"
         },
-      :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Nurse", age: "44", attitude: "worried"}
-      ]
+      :heroine_friends => []
    }
   }
 
